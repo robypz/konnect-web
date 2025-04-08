fb50853932788c3d86b2fb6153d2cad81d663aa6
robypz <robertyepez0208@hotmail.com>
2025-04-08 11:04:14 +0200

    feat: add header component and integrate into sidebar
    - Created a new HeaderComponent with a responsive navigation bar.
    - Integrated HeaderComponent into SidebarComponent, replacing the previous button for opening the sidebar.
    - Updated SidebarComponent's HTML and TypeScript files to include the new header.
    - Added basic styling for the HeaderComponent.
    - Created unit tests for HeaderComponent to ensure functionality.

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../header/header.component";
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
