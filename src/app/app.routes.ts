e1d136185609d6c5d6bb2e01532086a1390914c4
robypz <robertyepez0208@hotmail.com>
2025-04-08 18:07:41 +0200

    feat: add event, project, task, and tasks components with routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,18 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { EventsComponent } from './events/events.component';
 
 export const routes: Routes = [
   {path: '', component: HomeComponent},
+
   {path: 'dashboard', component: DashboardComponent},
+
+  {path: 'projects', component: ProjectsComponent},
+
+  {path: 'tasks', component: TasksComponent},
+
+  {path: 'events', component: EventsComponent},
 ];
