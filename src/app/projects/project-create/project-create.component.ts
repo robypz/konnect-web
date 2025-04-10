cc0be0c201087ba5a899a557f1178848b893387c
robypz <robertyepez0208@hotmail.com>
2025-04-10 10:04:48 +0200

    feat: implement project creation modal and enhance project display components

---
+++ src/app/projects/project-create/project-create.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-create',
+  imports: [],
+  templateUrl: './project-create.component.html',
+  styleUrl: './project-create.component.scss'
+})
+export class ProjectCreateComponent {
+
+}
--- src/app/projects/project-index/project-index.component.ts
+++ src/app/projects/project-index/project-index.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { ProjectComponent } from "../project/project.component";
+import { ProjectCreateComponent } from "../project-create/project-create.component";
 
 @Component({
   selector: 'app-project-index',
-  imports: [],
+  imports: [ProjectComponent, ProjectCreateComponent],
   templateUrl: './project-index.component.html',
   styleUrl: './project-index.component.scss'
 })
