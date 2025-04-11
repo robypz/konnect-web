8fa649c21d435755e894096b962828a155f33b65
robypz <robertyepez0208@hotmail.com>
2025-04-11 17:44:38 +0200

    feat: define Employee, Task, and User models with properties for data management

--- src/app/employees/shared/employee.model.ts
+++ src/app/employees/shared/employee.model.ts
@@ -1,2 +1,13 @@
+import { User } from "../../core/models/user.model";
+import { Project } from "../../projects/shared/project.model";
+import { Task } from "../../tasks/shared/task.model";
+
 export interface Employee {
+  id: string; // Unique identifier for the employee
+  created_at: Date; // Timestamp when the employee was created
+  updated_at: Date; // Timestamp when the employee was last updated
+  tasks?: Task[]; // Optional array of tasks associated with the employee
+  projects?: Project[]; // Optional array of projects associated with the employee
+  user_id: string; // ID of the user associated with the employee
+  user : User;
 }
--- src/app/tasks/shared/task.model.ts
+++ src/app/tasks/shared/task.model.ts
@@ -1,2 +1,15 @@
+import { Employee } from "../../employees/shared/employee.model";
+import { Project } from "../../projects/shared/project.model";
+
 export interface Task {
+  id: string; // Unique identifier for the task
+  name: string; // Title of the task
+  description: string; // Description of the task
+  completed_at: boolean; // Indicates if the task is completed
+  created_at: Date; // Timestamp when the task was created
+  updated_at: Date; // Timestamp when the task was last updated
+  employee_id: string; // ID of the employee assigned to the task
+  employee : Employee; // Employee object associated with the task
+  project_id: string; // ID of the project to which the task belongs
+  project : Project; // Project object associated with the task
 }
