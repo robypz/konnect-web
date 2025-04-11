d790a1d91e4c40603d61f1b10dc83c38302a68ed
robypz <robertyepez0208@hotmail.com>
2025-04-11 18:37:51 +0200

    feat: implement EmployeeService with CRUD operations for employee management

--- src/app/employees/shared/employee.service.ts
+++ src/app/employees/shared/employee.service.ts
@@ -1,9 +1,78 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable, signal } from '@angular/core';
+import { Employee } from './employee.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
+  private http = inject(HttpClient);
+  private employees = signal<Employee[]>([]);
+  private employee = signal<Employee | null>(null);
+  private errors = signal<string[]>([]);
 
   constructor() { }
+
+  index(){
+    this.http.get<Employee[]>('http://localhost:3000/employees').subscribe({
+      next: (employees) => {
+        this.employees.set(employees);
+      },
+      error: (error) => {
+        this.errors.set([error]);
+      }
+    });
+  }
+
+  show(id: number){
+    this.http.get<Employee>(`http://localhost:3000/employees/${id}`).subscribe({
+      next: (employee) => {
+        this.employee.set(employee);
+      },
+      error: (error) => {
+        this.errors.set([error]);
+      }
+    });
+  }
+
+  store(employee: Employee){
+    this.http.post<Employee>('http://localhost:3000/employees', employee).subscribe({
+      next: (employee) => {
+        this.employees.update((employees) => [...employees, employee]);
+      },
+      error: (error) => {
+        this.errors.set([error]);
+      }
+    });
+  }
+
+  update(employee: Employee){
+    this.http.put<Employee>(`http://localhost:3000/employees/${employee.id}`, employee).subscribe({
+      next: (employee) => {
+        this.employees.update((employees) => {
+          const index = employees.findIndex(e => e.id === employee.id);
+          employees[index] = employee;
+          return [...employees];
+        });
+      },
+      error: (error) => {
+        this.errors.set([error]);
+      }
+    });
+  }
+
+  destroy(id: string){
+    this.http.delete<Employee>(`http://localhost:3000/employees/${id}`).subscribe({
+      next: () => {
+        this.employees.update((employees) => {
+          const index = employees.findIndex(e => e.id === id);
+          employees.splice(index, 1);
+          return [...employees];
+        });
+      },
+      error: (error) => {
+        this.errors.set([error]);
+      }
+    });
+  }
 }
