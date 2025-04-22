a2b690364d847fbb6e0b77c94456f5ae69f116a1
robypz <robertyepez0208@hotmail.com>
2025-04-22 09:32:58 +0200

    feat: enhance carousel functionality with item IDs and navigation controls

--- src/app/posts/post/post.component.ts
+++ src/app/posts/post/post.component.ts
@@ -1,5 +1,5 @@
 import { afterNextRender, Component } from '@angular/core';
-import { Carousel } from 'flowbite';
+import { Carousel, CarouselInterface, CarouselItem } from 'flowbite';
 import { CommentsComponent } from "../../comments/comments.component";
 
 @Component({
@@ -9,10 +9,42 @@ import { CommentsComponent } from "../../comments/comments.component";
   styleUrl: './post.component.scss'
 })
 export class PostComponent {
-  carousel : Carousel | null = null;
   constructor() {
     afterNextRender(() => {
-      this.carousel = new Carousel(document.getElementById('animation-carousel') as HTMLElement);
+      const items: CarouselItem[] = [
+        {
+          position: 0,
+          el: document.getElementById('carousel-item-1') as HTMLElement,
+        },
+        {
+          position: 1,
+          el: document.getElementById('carousel-item-2') as HTMLElement,
+        },
+        {
+          position: 2,
+          el: document.getElementById('carousel-item-3') as HTMLElement,
+        },
+        {
+          position: 3,
+          el: document.getElementById('carousel-item-4') as HTMLElement,
+        },
+        {
+          position: 4,
+          el: document.getElementById('carousel-item-5') as HTMLElement,
+        },
+      ];
+      const carousel: CarouselInterface = new Carousel(document.getElementById('animation-carousel') as HTMLElement, items);
+      // set event listeners for prev and next buttons
+      const $prevButton = document.getElementById('data-carousel-prev') as HTMLElement;
+      const $nextButton = document.getElementById('data-carousel-next') as HTMLElement;
+
+      $prevButton.addEventListener('click', () => {
+        carousel.prev();
+      });
+
+      $nextButton.addEventListener('click', () => {
+        carousel.next();
+      });
     });
   }
 }
