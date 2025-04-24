5edb28c59c752a150b1f710ade62b162bee73e80
robypz <robertyepez0208@hotmail.com>
2025-04-24 13:48:57 +0200

    feat: implement employee creation form with validation and profile photo upload

--- src/app/employees/employee-create/employee-create.component.ts
+++ src/app/employees/employee-create/employee-create.component.ts
@@ -1,14 +1,27 @@
 import { afterNextRender, Component } from '@angular/core';
 import { Modal } from 'flowbite';
+import { EmailValidator, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-employee-create',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './employee-create.component.html',
   styleUrl: './employee-create.component.scss'
 })
 export class EmployeeCreateComponent {
   modal : Modal | null = null;
+
+  createEmployeeForm = new FormGroup({
+    name: new FormControl('', { nonNullable: true, validators: [Validators.required] }),
+    last_name: new FormControl('', { nonNullable: true, validators: [Validators.required] }),
+    password: new FormControl('', { nonNullable: true, validators: [Validators.required]}),
+    password_confirmation: new FormControl('', { nonNullable: true, validators: [Validators.required] }),
+    email: new FormControl('',{ nonNullable: true, validators: [Validators.required,Validators.email] }),
+    deparment_id: new FormControl('',{ nonNullable: true, validators: [Validators.required] }),
+    job: new FormControl('',{ nonNullable: true, validators: [Validators.required] }),
+    profile_photo : new FormControl<File|null>(null, { nonNullable: true, validators: [Validators.required] }),
+  });
+
   constructor() {
     afterNextRender(() => {
       this.modal = new Modal(document.getElementById('create-employee-modal') as HTMLElement);
@@ -19,5 +32,23 @@ export class EmployeeCreateComponent {
   }
   closeModal(){
     this.modal?.hide();
+  }
+
+  profilePhotoUpload(event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      const file = input.files[0];
+      this.createEmployeeForm.patchValue({ profile_photo: file });
+    }
+  }
+
+  createEmployee(){
+    if (this.createEmployeeForm.valid) {
+      console.log(this.createEmployeeForm.value as FormData);
+    }
+    else{
+      console.log(this.createEmployeeForm.errors);
+    }
+
   }
 }
