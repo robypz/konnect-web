b7cdb996f71ddcf90741c5f68b226be60bd2d8e8
Robert Yepez <robertyepez0208@hotmail.com>
2025-05-12 09:57:27 +0200

    `Update Angular components to implement OnInit lifecycle hook and add Flowbite carousel and tabs functionality`

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { afterNextRender, Component } from '@angular/core';
+import { afterNextRender, Component, OnInit } from '@angular/core';
 import { Tabs } from 'flowbite';
 import { PostsComponent } from '../posts/posts.component';
 import { ProjectsComponent } from "../projects/projects.component";
@@ -10,7 +10,7 @@ import { EventsComponent } from "../events/events.component";
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit{
 
   constructor() {
     /*afterNextRender(() => {
@@ -41,5 +41,33 @@ export class DashboardComponent {
       const tab = new Tabs(tabElement, tabElements);
     }
     );*/
+  }
+
+  ngOnInit(): void {
+    const tabElement = document.getElementById('default-styled-tab') as HTMLElement;
+      const tabElements = [
+        {
+          id: 'profile',
+          triggerEl: document.getElementById('profile-styled-tab') as HTMLElement,
+          targetEl: document.getElementById('styled-profile') as HTMLElement,
+        },
+        {
+          id: 'dashboard',
+          triggerEl: document.getElementById('dashboard-styled-tab') as HTMLElement,
+          targetEl: document.getElementById('styled-dashboard') as HTMLElement,
+        },
+        {
+          id: 'settings',
+          triggerEl: document.getElementById('settings-styled-tab') as HTMLElement,
+          targetEl: document.getElementById('styled-settings') as HTMLElement,
+        },
+        {
+          id: 'contacts',
+          triggerEl: document.getElementById('contacts-styled-tab') as HTMLElement,
+          targetEl: document.getElementById('styled-contacts') as HTMLElement,
+        },
+      ];
+
+      const tab = new Tabs(tabElement, tabElements);
   }
 }
--- src/app/posts/post/post.component.ts
+++ src/app/posts/post/post.component.ts
@@ -1,4 +1,4 @@
-import { afterNextRender, Component } from '@angular/core';
+import { afterNextRender, Component, OnInit } from '@angular/core';
 import { Carousel, CarouselInterface, CarouselItem } from 'flowbite';
 import { CommentsComponent } from "../../comments/comments.component";
 
@@ -8,7 +8,7 @@ import { CommentsComponent } from "../../comments/comments.component";
   templateUrl: './post.component.html',
   styleUrl: './post.component.scss'
 })
-export class PostComponent {
+export class PostComponent implements OnInit {
   constructor() {
     afterNextRender(() => {
       const items: CarouselItem[] = [
@@ -46,5 +46,42 @@ export class PostComponent {
         carousel.next();
       });
     });
+  }
+
+  ngOnInit(): void {
+    const items: CarouselItem[] = [
+        {
+          position: 0,
+          el: document.getElementById('carousel-item-1') as HTMLElement,
+        },
+        {
+          position: 1,
+          el: document.getElementById('carousel-item-2') as HTMLElement,
+        },
+        {
+          position: 2,
+          el: document.getElementById('carousel-item-3') as HTMLElement,
+        },
+        {
+          position: 3,
+          el: document.getElementById('carousel-item-4') as HTMLElement,
+        },
+        {
+          position: 4,
+          el: document.getElementById('carousel-item-5') as HTMLElement,
+        },
+      ];
+      const carousel: CarouselInterface = new Carousel(document.getElementById('animation-carousel') as HTMLElement, items);
+      // set event listeners for prev and next buttons
+      const $prevButton = document.getElementById('data-carousel-prev') as HTMLElement;
+      const $nextButton = document.getElementById('data-carousel-next') as HTMLElement;
+
+      $prevButton.addEventListener('click', () => {
+        carousel.prev();
+      });
+
+      $nextButton.addEventListener('click', () => {
+        carousel.next();
+      });
   }
 }
