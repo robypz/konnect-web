c659428c540eeb8561bb4b058e9d60b77b548ea4
Robert Yepez <robertyepez0208@hotmail.com>
2025-05-19 06:49:18 +0200

    feat: Update event creation modal to improve functionality and enhance form handling

--- src/app/events/event-create/event-create.component.ts
+++ src/app/events/event-create/event-create.component.ts
@@ -1,23 +1,38 @@
-import { afterNextRender, Component } from '@angular/core';
-import { Modal } from 'flowbite';
+import { /*afterNextRender,*/ Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+//import { Modal } from 'flowbite';
 
 @Component({
   selector: 'app-event-create',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './event-create.component.html',
   styleUrl: './event-create.component.scss'
 })
 export class EventCreateComponent {
-  modal : Modal | null = null;
+  //modal : Modal | null = null;
+
+  createEventForm = new FormGroup({
+    name: new FormControl('',[Validators.required]),
+    description: new FormControl('',[Validators.required]),
+    date: new FormControl('',[Validators.required,]),
+    time: new FormControl('',[Validators.required]),
+    start_time: new FormControl('',[Validators.required]),
+    end_time: new FormControl('',[Validators.required]),
+    location: new FormControl('',[Validators.required]),
+  });
+
+
+
   constructor() {
-    afterNextRender(() => {
+    /*afterNextRender(() => {
       this.modal = new Modal(document.getElementById('create-event-modal') as HTMLElement);
-    });
+    });*/
+
   }
-  openModal(){
+  /*openModal(){
     this.modal?.show();
   }
   closeModal(){
     this.modal?.hide();
-  }
+  }*/
 }
