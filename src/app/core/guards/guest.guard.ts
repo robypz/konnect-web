fa9fc21ce3d73d685b943ff5845bd5a324e1f4a3
Robert Yepez <robertyepez0208@hotmail.com>
2025-08-05 12:34:13 +0200

    "add image to home page"

--- src/app/core/guards/guest.guard.ts
+++ src/app/core/guards/guest.guard.ts
@@ -1,7 +1,6 @@
 import { inject } from '@angular/core';
 import { CanActivateFn } from '@angular/router';
 import { AuthService } from '../auth/shared/auth.service';
-import { Router } from 'express';
 
 export const guestGuard: CanActivateFn = (route, state) => {
   const auth = inject(AuthService).auth();
